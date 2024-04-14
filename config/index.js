59e4e6051d5258e02ab12a4479aa60a84ec72ca9
sbkim <sbkim@aimmed.com>
2024-04-14 15:55:26 +0900

    [feat] 기본 환경설정 기능 추가     - dotenv 라이브러리 추가     - .env 파일을 읽어 process.env에 세팅

--- config/index.js
+++ config/index.js
@@ -2,3 +2,16 @@
  * index.js
  * 
  */
+const dotenv = require('dotenv');
+// .env 파일을 읽고 process.env에 세팅합니다.
+dotenv.config();
+
+const DEFAULT_PORT = 3000;
+
+const config = {
+  port: process.env.PORT || DEFAULT_PORT,
+  // Database
+  // Other..
+};
+
+module.exports = config;
--- server.js
+++ server.js
@@ -2,9 +2,13 @@
  * server.js
  *  서버 초기화 및 실행 파일
  */
+// Libs
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes/index');
+
+// Configs
+const config = require('./config');
 const db = require('./config/database');
 
 const app = express();
@@ -18,7 +22,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = config.port;
 app.listen(PORT, () => {
   console.log(`Server is running`);
 });
