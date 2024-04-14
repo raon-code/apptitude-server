5fe31f2407c72143ded7f3c3d9b1ddb67689b94b
sbkim <sbkim@aimmed.com>
2024-04-14 19:06:05 +0900

    [feat] sequelize 초기화 및 테스트 테이블 추가     - sequelize와 SQLite 연결     - 테스트 테이블 스키마 정보를 통해 sequelize 연동(models/test.js)     - 테스트를 위해 server.js 임의코드 작성         - 100개의 임의 데이터 생성         - 100개 데이터 조회 (/test)
    TODO: empty.js는 아직 개발 전 상태를 표시

--- config/database/index.js
+++ config/database/index.js
@@ -2,18 +2,19 @@
  * db.js
  *  데이터베이스 관련 설정
  */
-const config = require('../');  // config/index
+const ROOT_DIR = process.cwd();
+const config = require(ROOT_DIR + '/config');  // config/index
 
-let db;
+let db;       // db 인스턴스
 
 switch(config.nodeEnv) {
-  case 'dev': // 개발환경
+  case 'dev':   // 개발환경
     break; 
 
-  case 'prod': // 운영환경
+  case 'prod':  // 운영환경
     break; 
   
-  default: // 로컬환경
+  default:      // 로컬환경
     db = require('./sqlite');
     break;
 }
--- server.js
+++ server.js
@@ -2,14 +2,18 @@
  * server.js
  *  서버 초기화 및 실행 파일
  */
-// Libs
 const express = require('express');
 const bodyParser = require('body-parser');
-const routes = require('./routes/index');
+
+const ROOT_DIR = process.cwd();
 
 // Configs
-const config = require('./config');
-const db = require('./config/database');
+const config = require(ROOT_DIR + '/config');
+const db = require(ROOT_DIR + '/config/database');
+
+// API
+const sequelize = require(ROOT_DIR + '/models');
+const Test = require(ROOT_DIR + '/models/test');
 
 const app = express();
 
@@ -18,6 +22,71 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
+///////////////////////////////////////////////////////////////////////////
+/* Sequelize TEST(임시코드) */
+async function initialize() {
+  try {
+    await sequelize.sync();
+    console.log('모든 모델이 동기화되었습니다.');
+
+    const isEmpty = await isTestEmpty();
+    if(isEmpty) {
+      console.log('데이터가 비어있으므로 임의생성');
+      for(let i = 1; i <= 100; i++) {
+        await createTest(`앱티튜트 테스트 제목 ${i}`, `앱티튜트 테스트 내용 ${i}`);
+      }
+    }
+    console.log('서버 초기화 성공');
+  } catch(error) {
+    console.error('서버 초기화 중 오류 발생:', error);
+  }
+}
+initialize();
+
+async function isTestEmpty() {
+  try {
+    const tests = await Test.findAll();
+    return tests.length === 0;
+  } catch (error) {
+    console.error('에러 발생:', error);
+    return false;
+  }
+}
+
+async function createTest(title, contents) {
+  try {
+    const newTest = await Test.create({
+      dateTime: new Date(),
+      title: title,
+      contents: contents
+    });
+    console.log('새로운 데이터가 성공적으로 추가되었습니다:', newTest.toJSON());
+  } catch (error) {
+    console.error('데이터 추가 중 오류 발생:', error);
+  }
+}
+
+app.get('/test', async (req, res) => {
+  try {
+    const testList = await Test.findAll();
+    // Sequelize 모델 인스턴스 배열을 JSON으로 변환
+    const dataList = testList.map(test => test.toJSON());  // 각 모델 인스턴스를 JSON 객체로 변환
+    res.json({
+      'message': 'success',
+      'data': dataList
+    });
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).json({
+      'message': 'error',
+      'data': error.message
+    });
+  }
+});
+/* Sequelize TEST */
+///////////////////////////////////////////////////////////////////////////
+
+// Home Page
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
