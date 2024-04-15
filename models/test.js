05840e0d7ade62de9587696f7cabc1691db461d3
sbkim <sbkim@aimmed.com>
2024-04-15 14:39:22 +0900

    [refactor] prettier 설정파일 추가

--- models/test.js
+++ models/test.js
@@ -10,35 +10,38 @@ const isForce = require(ROOT_DIR + '/config').models.forceSync;
 
 class Test extends Model {}
 
-Test.init({
-  id: {
-    type: DataTypes.BIGINT,
-    autoIncrement: true,
-    primaryKey: true,
-    allowNull: false,
-    comment: '아이디'
+Test.init(
+  {
+    id: {
+      type: DataTypes.BIGINT,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+      comment: '아이디'
+    },
+    dateTime: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      comment: '일시'
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      comment: '제목'
+    },
+    contents: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      comment: '내용'
+    }
   },
-  dateTime: {
-    type: DataTypes.DATE,
-    allowNull: false,
-    comment: '일시'
-  },
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    comment: '제목'
-  },
-  contents: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-    comment: '내용'
+  {
+    sequelize, // 인스턴스 전달
+    modelName: 'test', // 모델 이름 지정
+    comment: '테스트', // 테이블에 대한 설명
+    timestamps: false // createdAt, updatedAt 컬럼을 자동으로 추가하지 않음
   }
-}, {
-  sequelize, // 인스턴스 전달
-  modelName: 'test', // 모델 이름 지정
-  comment: '테스트', // 테이블에 대한 설명
-  timestamps: false, // createdAt, updatedAt 컬럼을 자동으로 추가하지 않음
-});
+);
 
 // sequelize와 동기화
 async function syncModel() {
--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const Test = require(ROOT_DIR + '/models/test');
 const app = express();
 
 // 서버 기본 설정
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
@@ -31,14 +31,14 @@ async function initialize() {
     console.log('모든 모델이 동기화되었습니다.');
 
     const isEmpty = await isTestEmpty();
-    if(isEmpty) {
+    if (isEmpty) {
       console.log('데이터가 비어있으므로 임의생성');
-      for(let i = 1; i <= 100; i++) {
+      for (let i = 1; i <= 100; i++) {
         await createTest(`앱티튜트 테스트 제목 ${i}`, `앱티튜트 테스트 내용 ${i}`);
       }
     }
     console.log('서버 초기화 성공');
-  } catch(error) {
+  } catch (error) {
     console.error('서버 초기화 중 오류 발생:', error);
   }
 }
@@ -71,16 +71,16 @@ app.get('/test', async (req, res) => {
   try {
     const testList = await Test.findAll();
     // Sequelize 모델 인스턴스 배열을 JSON으로 변환
-    const dataList = testList.map(test => test.toJSON());  // 각 모델 인스턴스를 JSON 객체로 변환
+    const dataList = testList.map((test) => test.toJSON()); // 각 모델 인스턴스를 JSON 객체로 변환
     res.json({
-      'message': 'success',
-      'data': dataList
+      message: 'success',
+      data: dataList
     });
   } catch (error) {
     console.error('Error fetching data:', error);
     res.status(500).json({
-      'message': 'error',
-      'data': error.message
+      message: 'error',
+      data: error.message
     });
   }
 });
