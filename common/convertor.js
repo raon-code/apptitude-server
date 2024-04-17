f2f2aa50b7ab01520f96ed92994dc35a588cdf6b
sbkim <sbkim@aimmed.com>
2024-04-17 09:38:02 +0900

    [feat] module-alias 패키지 설치     - require path 관리에 유용

--- common/convertor.js
+++ common/convertor.js
@@ -4,24 +4,3 @@
  */
 const ROOT_DIR = process.cwd();
 const logger = require(ROOT_DIR + '/config/logger');
-
-const KST_OFFSET = 9 * 60 * 60000;
-
-/**
- *
- * @param {Date} utcDate
- * @returns
- */
-function toKSTDate(utcDate) {}
-
-/**
- *
- * @param {Date} kstDate
- * @returns
- */
-function toUTCDate(kstDate) {}
-
-module.exports = {
-  toKSTDate,
-  toUTCDate
-};
