eb1b606e61fa9b469c538acff9f5db4a11e78a6a
sbkim <sbkim@aimmed.com>
2024-04-19 14:52:12 +0900

    [refactor] crypto 주석 추가

--- common/crypto.js
+++ common/crypto.js
@@ -8,14 +8,30 @@ const config = require('@/config').crypto;
 const logger = require('@/config/logger');
 
 const algorithm = config.algorithm;
-const key = Buffer.from(sha256Hash(config.seedKey), 'hex');
+const key = Buffer.from(hashEncrypt(config.seedKey), 'hex');
 
-function sha256Hash(input) {
+/**
+ * 해시알고리즘을 사용한 암호화를 합니다.
+ * (복호화 불가능)
+ *
+ * @param {string} input 문자열
+ * @returns 해시 암호화된 문자열
+ */
+function hashEncrypt(input) {
   const hash = crypto.createHash('sha256');
   hash.update(input);
   return hash.digest('hex');
 }
 
+/**
+ * 문자열을 암호화합니다.
+ *
+ * 암호화패턴
+ *   <랜덤IV값>:<암호화문자열>
+ *
+ * @param {string} text 문자열
+ * @returns 암호화된 문자열
+ */
 function encrypt(text) {
   const iv = crypto.randomBytes(16); // 매번 새로운 IV를 사용
 
@@ -27,6 +43,18 @@ function encrypt(text) {
   return result;
 }
 
+/**
+ * 암호화된 문자열을 복호화합니다.
+ *
+ * 암호화패턴
+ *   <랜덤IV값>:<암호화문자열>
+ *
+ * ':' 앞에 <랜덤IV값>을 먼저 가져옵니다.
+ * 그리고, <랜덤IV값>을 이용해 ':' 뒤에 <암호화문자열>을 복호화 합니다.
+ *
+ * @param {string} input 암호화된 문자열
+ * @returns 복호화된 문자열
+ */
 function decrypt(input) {
   const parts = input.split(':'); // IV를 분리
   const iv = Buffer.from(parts.shift(), 'base64');
@@ -46,6 +74,7 @@ function decrypt(input) {
 // logger.warn(decrypt(encrypted));
 
 module.exports = {
+  hashEncrypt,
   encrypt,
   decrypt
 };
