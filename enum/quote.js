e77d2c66b076281c8b8b9c5c518f8e30d040a85e
sbkim <sbkim@aimmed.com>
2024-04-25 21:46:27 +0900

    quote 수정

--- enum/quote.js
+++ enum/quote.js
@@ -1,5 +1,10 @@
 const QUOTE = {
-  QT000: commonData('QT000', '일하지 않는 자, 먹지도 말라\n- 데살로니카 후서')
+  QT000: {
+    common: {
+      codeValue: 'QT000',
+      description: '일하지 않는 자, 먹지도 말라\n- 데살로니카 후서'
+    }
+  }
 };
 
 module.exports = QUOTE;
