2204774085aa69b3e684c3184f4e40922dcfbebc
sbkim <sbkim@aimmed.com>
2024-05-10 10:17:44 +0900

    [feat] jwt 토큰을 이용한 인증 로직 추가     - 인증이 잘되는지 테스트용 컨트롤러 추가(auth-controller.js)         - '/api/auth/protected'     - passport를 이용하여 jwt 검증 후 컨트롤러 로직 수행     - TODO         - 토큰 갱신 로직         - 올바른 페이로드인지 검증

--- middleware/auth-handler.js
+++ middleware/auth-handler.js
@@ -8,25 +8,46 @@ const ExtractJwt = require('passport-jwt').ExtractJwt;
 
 const logger = require('@/config/logger');
 const config = require('@/config');
+const { StatusCodes } = require('http-status-codes');
+const { UnauthorizeError } = require('@/error');
+const response = require('@/common/response');
 
 const passportOption = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: config.jwt.secret
+  secretOrKey: config.jwt.secret,
+  algorithms: config.jwt.algorithm
 };
 
 const authHandler = passport.use(
-  new JWTStrategy(passportOption, async (payload, done) => {
-    try {
-      // 인증로직
-      const user = 1; // User.findById(payload.id);
-      if (user) return done(null, user);
-    } catch (error) {
-      return done(error);
+  new JWTStrategy(passportOption, async (jwtPayload, done) => {
+    // jwt_payload는 검증이 완료된 후의 payload이며,
+    // 여기서 사용자의 추가적인 인증 로직을 구현할 수 있습니다.
+    // 예를 들어, jwt_payload의 사용자 정보를 바탕으로 데이터베이스를 조회할 수 있습니다.
+
+    // 인증로직
+    if (jwtPayload.id !== 1) {
+      return done(null, false);
     }
+
+    // TODO: 토큰 갱신 로직
+
+    return done(null, jwtPayload);
   })
 );
 
-const authMiddleware = authHandler.authenticate('jwt', { session: false });
+const authMiddleware = (req, res, next) => {
+  authHandler.authenticate('jwt', { session: false }, (err, user, info) => {
+    // 인증로직에서 에러 발생 또는 권한이 없는 경우
+    if (err || !user) {
+      // TODO: 에러 로그로 잘못된 접근에 대한 기록이 필요해보임
+      response(res, StatusCodes.UNAUTHORIZED, '권한없음');
+      return;
+    }
+
+    req.user = user;
+    next();
+  })(req, res, next);
+};
 
 module.exports = {
   authMiddleware,
