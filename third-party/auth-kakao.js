9d704bc4793c1ad03688882e43803ed8ffeb6f2e
sbkim <sbkim@aimmed.com>
2024-05-13 14:10:52 +0900

    [feat] 카카오 API 설정 추가

---
+++ third-party/auth-kakao.js
@@ -0,0 +1,12 @@
+// urls: {
+//     // 인가 코드받기
+//     getAuth: 'https://kauth.kakao.com/oauth/authorize',
+//     // 토큰 발급하기
+//     generateToken: 'https://kauth.kakao.com/oauth/token',
+//     // 토큰 정보보기
+//     getTokenInfo: 'https://kapi.kakao.com/v1/user/access_token_info',
+//     // 유저 정보보기
+//     getUserInfo: 'https://kapi.kakao.com/v2/user/me',
+//     // 로그아웃
+//     logout: 'https://kapi.kakao.com/v1/user/logout'
+//   },
