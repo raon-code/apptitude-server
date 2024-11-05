ef63eced11a3483167b84d216045fd7059efc8f8
sbkim <sbkim@aimmed.com>
2024-11-05 10:57:06 +0900

    [docs] 커밋 내용에 대한 피드백 추가 (ex. TODO: 피드백 내용 추가)

--- routes/battle-detail-controller.js
+++ routes/battle-detail-controller.js
@@ -23,17 +23,19 @@ router.use(authMiddleware);
 // 배틀 상세 생성
 router.post('/', createBattleDetail);
 async function createBattleDetail(req, res) {
+  // TODO: exception-handler.js 미들웨어를 통해 전역으로 예외를 catch하고 있으므로,
+  //       try-catch로 감쌀 필요 없음
   try {
     const userId = req.user.id;
     const battleId = req.params.battleId;
 
-    // TODO: createBattleDetailDTO 추가
     const createBattleDetailDTO = CreateBattleDetailDTO.fromPlainObject(
       req.body
     );
     createBattleDetailDTO.validate();
 
-    // TODO: createBattleDetail 서비스 추가: 배틀 상세 생성
+    // TODO: 해당 배틀에 참가한 사용자인지 확인 필요
+
     const newBattleDetail = await battleDetailService.createBattleDetail(
       createBattleDetailDTO
     );
@@ -47,9 +49,11 @@ async function createBattleDetail(req, res) {
   }
 }
 
-// TODO: 배틀 상세 목록 조회
+// 배틀 상세 목록 조회
 router.get('/', getBattleDetailList);
 async function getBattleDetailList(req, res) {
+  // TODO: exception-handler.js 미들웨어를 통해 전역으로 예외를 catch하고 있으므로,
+  //       try-catch로 감쌀 필요 없음
   try {
     const battleId = req.params.battleId;
 
@@ -64,7 +68,8 @@ async function getBattleDetailList(req, res) {
     if (statusType) filter.statusType = statusType;
     if (detoxTime) filter.detoxTime = detoxTime;
 
-    // TODO: getBattleDetailList 서비스 추가: 배틀 상세 목록 조회
+    // TODO: 해당 배틀에 참가한 사용자인지 확인 필요
+
     const battleDetailList = await battleDetailService.getBattleDetailList(
       battleId
     );
@@ -74,17 +79,22 @@ async function getBattleDetailList(req, res) {
   }
 }
 
-// TODO: 배틀 상세 조회
-router.get('/:detailId(\\d+)', getBattleDetail);
+// 배틀 상세 조회
+router.get('/:battleDetailId(\\d+)', getBattleDetail);
 async function getBattleDetail(req, res) {
+  // TODO: exception-handler.js 미들웨어를 통해 전역으로 예외를 catch하고 있으므로,
+  //       try-catch로 감쌀 필요 없음
   try {
-    const battleDetailId = req.params.detailId;
+    const battleDetailId = req.params.battleDetailId;
 
-    // TODO: getBattleDetail 서비스 추가: 배틀 상세 조회
+    // TODO: 해당 배틀에 참가한 사용자인지 확인 필요
+
     const battleDetail = await battleDetailService.getBattleDetail(
       battleDetailId
     );
+
     if (!battleDetail) {
+      // TODO: 에러를 throw하면 미들웨어가 이를 캐치하여 처리하므로, 직접 response를 호출할 필요 없음
       return response(
         res,
         StatusCodes.NOT_FOUND,
@@ -97,17 +107,19 @@ async function getBattleDetail(req, res) {
   }
 }
 
-// TODO: 배틀 상세 수정
+// 배틀 상세 수정
 router.patch('/:detailId(\\d+)', updateBattleDetail);
 async function updateBattleDetail(req, res) {
+  // TODO: exception-handler.js 미들웨어를 통해 전역으로 예외를 catch하고 있으므로,
+  //       try-catch로 감쌀 필요 없음
   try {
     const battleDetailId = req.params.detailId;
 
-    // TODO: updateBattleDetail 서비스 추가: 배틀 상세 수정
     const battleDetail = await battleDetailService.getBattleDetail(
       battleDetailId
     );
     if (!battleDetail) {
+      // TODO: 에러를 throw하면 미들웨어가 이를 캐치하여 처리하므로, 직접 response를 호출할 필요 없음
       return response(
         res,
         StatusCodes.NOT_FOUND,
@@ -115,13 +127,16 @@ async function updateBattleDetail(req, res) {
       );
     }
 
+    // TODO: 검증 후 조회하면 데이터베이스 리소스 조회를 줄일 수 있음
     // DTO 생성 및 검증 (수정 시에는 필요한 필드만 검증)
     const updateData = { ...req.body };
+
+    // TODO: updateBattleDetailDTO를 추가하여 사용해야 함
+    //       업데이트 로직은 생성 로직과 다를 수 있으므로..
     const createBattleDetailDTO =
       CreateBattleDetailDTO.fromPlainObject(updateData);
     createBattleDetailDTO.validate();
 
-    // 배틀 상세 업데이트
     const updatedBattleDetail = await battleDetailService.updateBattleDetail(
       battleDetailId,
       updateData
